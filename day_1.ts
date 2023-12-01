92d5db634f26a8dbd6b2a9c8decaafa73e264f64
Chris Cowan <agentme49@gmail.com>
2023-11-30 21:34:55 -0800

    day 1 part 1

--- day_1.ts
+++ day_1.ts
@@ -1,13 +1,18 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
-import { runPart } from "https://deno.land/x/aocd@v1.4.3/mod.ts";
+import { runPart } from "https://deno.land/x/aocd@v1.5.0/mod.ts";
 
 function parse(input: string) {
-  return input.trimEnd().split("\n").map(Number);
+  return input.trimEnd().split("\n");
 }
 
 function part1(input: string): number {
   const items = parse(input);
-  throw new Error("TODO");
+  return items.map((line) => {
+    const onlyNumbers = line.replace(/[^0-9]/g, "");
+    const firstDigit = onlyNumbers[0];
+    const lastDigit = onlyNumbers[onlyNumbers.length - 1];
+    return Number(firstDigit) * 10 + Number(lastDigit);
+  }).reduce((a, b) => a + b, 0);
 }
 
 // function part2(input: string): number {
@@ -21,15 +26,14 @@ if (import.meta.main) {
 }
 
 const TEST_INPUT = `\
-6
-7
-8
-9
-10
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
 `;
 
 Deno.test("part1", () => {
-  assertEquals(part1(TEST_INPUT), 11);
+  assertEquals(part1(TEST_INPUT), 142);
 });
 
 // Deno.test("part2", () => {
