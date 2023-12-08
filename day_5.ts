755bd7df28d45827a729778c178c16978601099c
Chris Cowan <agentme49@gmail.com>
2023-12-08 01:35:40 -0800

    day 5 part 2

--- day_5.ts
+++ day_5.ts
@@ -1,6 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { runPart } from "https://deno.land/x/aocd@v1.5.1/mod.ts";
 import { StaticIntervalTree } from "npm:mnemonist@0.39.6";
+import { chunk } from "https://deno.land/std@0.208.0/collections/chunk.ts";
 
 interface InputData {
   seeds: number[];
@@ -14,10 +15,31 @@ interface InputMap {
   intervalTree: StaticIntervalTree<ConversionRange>;
 }
 
+function createInputMap(
+  from: string,
+  to: string,
+  ranges: ConversionRange[],
+): InputMap {
+  return {
+    from,
+    to,
+    ranges,
+    intervalTree: StaticIntervalTree.from(ranges, [
+      (range) => range.sourceStart,
+      rangeEnd,
+    ]),
+  };
+}
+
 interface ConversionRange {
-  destStart: number;
+  destStart?: number;
   sourceStart: number;
   length: number;
+}
+
+/** Gets the inclusive source end of a range */
+function rangeEnd(range: ConversionRange): number {
+  return range.sourceStart + range.length - 1;
 }
 
 function parse(input: string): InputData {
@@ -41,15 +63,7 @@ function parse(input: string): InputData {
         length: numbers[2],
       };
     });
-    return {
-      from,
-      to,
-      ranges,
-      intervalTree: StaticIntervalTree.from(ranges, [
-        (range) => range.sourceStart,
-        (range) => range.sourceStart + range.length - 1,
-      ]),
-    };
+    return createInputMap(from, to, ranges);
   });
   return { seeds, maps };
 }
@@ -58,7 +72,7 @@ function mapValue(value: number, inputMap: InputMap): number {
   const conversionRange =
     inputMap.intervalTree.intervalsContainingPoint(value)[0];
   if (conversionRange) {
-    return conversionRange.destStart + (value - conversionRange.sourceStart);
+    return conversionRange.destStart! + (value - conversionRange.sourceStart);
   } else {
     return value;
   }
@@ -75,14 +89,85 @@ function part1(input: string): number {
   return Math.min(...locations);
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function mapRange(
+  range: ConversionRange,
+  inputMap: InputMap,
+): ConversionRange[] {
+  const relevantConversionRanges = inputMap.intervalTree
+    .intervalsOverlappingInterval(range)
+    .toSorted((a, b) => a.sourceStart - b.sourceStart);
+  // Assuming none of the conversion ranges overlap with each other.
+
+  // Clip them to the original range.
+  const clippedRelevantConversionRanges = relevantConversionRanges.map(
+    (relevantConversionRange) => {
+      const sourceStart = Math.max(
+        relevantConversionRange.sourceStart,
+        range.sourceStart,
+      );
+      const sourceEnd = Math.min(
+        rangeEnd(relevantConversionRange),
+        rangeEnd(range),
+      );
+      return {
+        destStart: relevantConversionRange.destStart! +
+          (sourceStart - relevantConversionRange.sourceStart),
+        sourceStart,
+        length: sourceEnd - sourceStart + 1,
+      };
+    },
+  );
+
+  // Need to fill in the gaps between the conversion ranges so that all numbers
+  // in the original range get converted to something.
+  const effectiveConversionRanges: ConversionRange[] = [];
+  let currentPosition = range.sourceStart;
+  for (const relevantConversionRange of clippedRelevantConversionRanges) {
+    if (relevantConversionRange.sourceStart > currentPosition) {
+      effectiveConversionRanges.push({
+        destStart: currentPosition,
+        sourceStart: currentPosition,
+        length: relevantConversionRange.sourceStart - currentPosition,
+      });
+    }
+    effectiveConversionRanges.push(relevantConversionRange);
+    currentPosition = rangeEnd(relevantConversionRange) + 1;
+  }
+  if (currentPosition <= rangeEnd(range)) {
+    effectiveConversionRanges.push({
+      destStart: currentPosition,
+      sourceStart: currentPosition,
+      length: rangeEnd(range) - currentPosition + 1,
+    });
+  }
+
+  // Create ranges based on the applied conversions.
+  const results = effectiveConversionRanges.map((conversionRange) => ({
+    sourceStart: conversionRange.destStart! +
+      Math.max(0, range.sourceStart - conversionRange.sourceStart),
+    length: conversionRange.length,
+  }));
+
+  return results;
+}
+
+function part2(input: string): number {
+  const inputData = parse(input);
+  const seedRanges: ConversionRange[] = chunk(inputData.seeds, 2)
+    .map(([start, length]) => ({ sourceStart: start, length }));
+  const locationRanges = seedRanges.flatMap((seedRange) =>
+    inputData.maps.reduce(
+      (ranges, inputMap) =>
+        ranges.flatMap((range) => mapRange(range, inputMap)),
+      [seedRange],
+    )
+  );
+  return Math.min(...locationRanges.map((range) => range.sourceStart));
+}
 
 if (import.meta.main) {
   runPart(2023, 5, 1, part1);
-  // runPart(2023, 5, 2, part2);
+  runPart(2023, 5, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -125,6 +210,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 35);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 46);
+});
