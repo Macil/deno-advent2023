3f12ed589be11eb681ca7aa5ffed1174d94b88b8
Chris Cowan <agentme49@gmail.com>
2024-06-27 23:42:02 -0700

    day 8 progress

--- day_8.ts
+++ day_8.ts
@@ -32,11 +32,7 @@ function part1(inputStr: string): number {
   while (position !== "ZZZ") {
     const step = input.path[stepIndex];
     const children = input.nodes[position];
-    if (step === "L") {
-      position = children[0];
-    } else {
-      position = children[1];
-    }
+    position = step === "L" ? children[0] : children[1];
     steps++;
     stepIndex = (stepIndex + 1) % input.path.length;
   }
@@ -44,10 +40,25 @@ function part1(inputStr: string): number {
   return steps;
 }
 
-// function part2(inputStr: string): number {
-//   const input = parse(inputStr);
-//   throw new Error("TODO");
-// }
+function part2(inputStr: string): number {
+  const input = parse(inputStr);
+
+  let steps = 0;
+  let stepIndex = 0;
+  let positions = Object.keys(input.nodes).filter((key) => key.endsWith("A"));
+
+  while (!positions.every((pos) => pos.endsWith("Z"))) {
+    const step = input.path[stepIndex];
+    positions = positions.map((position) => {
+      const children = input.nodes[position];
+      return step === "L" ? children[0] : children[1];
+    });
+    steps++;
+    stepIndex = (stepIndex + 1) % input.path.length;
+  }
+
+  return steps;
+}
 
 if (import.meta.main) {
   runPart(2023, 8, 1, part1);
@@ -74,11 +85,24 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 `;
 
+const TEST_INPUT3 = `\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+`;
+
 Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 2);
   assertEquals(part1(TEST_INPUT2), 6);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT3), 6);
+});
