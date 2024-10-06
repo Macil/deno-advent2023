9185f76dfb94af45267ca0911fd92cef7531ca70
Chris Cowan <agentme49@gmail.com>
2024-10-05 20:00:14 -0700

    day 8 part 2 finished

--- day_8.ts
+++ day_8.ts
@@ -1,4 +1,5 @@
 import { assertEquals } from "@std/assert";
+import { lcm } from "@babia/tiny-math";
 import { runPart } from "@macil/aocd";
 
 type PathStep = "L" | "R";
@@ -25,44 +26,165 @@ function parse(input: string): Input {
 function part1(inputStr: string): number {
   const input = parse(inputStr);
 
-  let steps = 0;
-  let stepIndex = 0;
+  let stepCount = 0;
+  let stepPathIndex = 0;
   let position = "AAA";
 
   while (position !== "ZZZ") {
-    const step = input.path[stepIndex];
+    const step = input.path[stepPathIndex];
     const children = input.nodes[position];
     position = step === "L" ? children[0] : children[1];
-    steps++;
-    stepIndex = (stepIndex + 1) % input.path.length;
+    stepCount++;
+    stepPathIndex = (stepPathIndex + 1) % input.path.length;
   }
 
-  return steps;
+  return stepCount;
+}
+
+interface RecurringEncounter {
+  firstEncounterStep: number;
+  stepsUntilRevisit: number;
+}
+
+function calculateFirstCommonEncounter(
+  recurringEncounters: ReadonlyArray<RecurringEncounter>,
+): number {
+  if (
+    recurringEncounters.some((e) =>
+      e.firstEncounterStep !== e.stepsUntilRevisit
+    )
+  ) {
+    throw new Error(
+      "This implementation only supports goals that are re-encountered at a fixed interval.",
+    );
+  }
+  return lcm(...recurringEncounters.map((e) => e.stepsUntilRevisit));
+}
+
+Deno.test("calculateFirstCommonEncounter", async (t) => {
+  await t.step("basic", () => {
+    assertEquals(
+      calculateFirstCommonEncounter([
+        { firstEncounterStep: 2, stepsUntilRevisit: 2 },
+        { firstEncounterStep: 3, stepsUntilRevisit: 3 },
+      ]),
+      6,
+    );
+    // assertEquals(
+    //   calculateFirstCommonEncounter([
+    //     { firstEncounterStep: 2, stepsUntilRevisit: 2 },
+    //     { firstEncounterStep: 5, stepsUntilRevisit: 3 },
+    //   ]),
+    //   8,
+    // );
+  });
+  await t.step("large values", () => {
+    assertEquals(
+      calculateFirstCommonEncounter([
+        { firstEncounterStep: 21409, stepsUntilRevisit: 21409 },
+        { firstEncounterStep: 14363, stepsUntilRevisit: 14363 },
+        { firstEncounterStep: 15989, stepsUntilRevisit: 15989 },
+        { firstEncounterStep: 16531, stepsUntilRevisit: 16531 },
+        { firstEncounterStep: 19241, stepsUntilRevisit: 19241 },
+        { firstEncounterStep: 19783, stepsUntilRevisit: 19783 },
+      ]),
+      21165830176709,
+    );
+  });
+});
+
+function calculateFirstCommonEncounterAmongPathers(
+  recurringGoalEncountersByPather: ReadonlyArray<
+    ReadonlyArray<RecurringEncounter>
+  >,
+): number {
+  if (
+    recurringGoalEncountersByPather.some((encountersSet) =>
+      encountersSet.length !== 1
+    )
+  ) {
+    throw new Error(
+      "This implementation only supports paths that encounter a single goal repeatedly.",
+    );
+  }
+  return calculateFirstCommonEncounter(
+    recurringGoalEncountersByPather.map((e) => e[0]!),
+  );
+}
+
+interface PathFollower {
+  position: string;
+  firstGoalEncounters: Map<string, number>;
+  /**
+   * This will be set to the complete Map once the pather has recognized it has
+   * entered a loop.
+   */
+  recurringGoalEncounters: Map<string, RecurringEncounter> | undefined;
+}
+
+function isPositionGoal(position: string): boolean {
+  return position.endsWith("Z");
 }
 
 function part2(inputStr: string): number {
   const input = parse(inputStr);
 
-  let steps = 0;
-  let stepIndex = 0;
-  let positions = Object.keys(input.nodes).filter((key) => key.endsWith("A"));
+  let stepCount = 0;
+  let stepPathIndex = 0;
+  const pathers: PathFollower[] = Object.keys(input.nodes)
+    .filter((key) => key.endsWith("A"))
+    .map((key) => ({
+      position: key,
+      firstGoalEncounters: new Map(),
+      recurringGoalEncounters: undefined,
+    }));
 
-  while (!positions.every((pos) => pos.endsWith("Z"))) {
-    const step = input.path[stepIndex];
-    positions = positions.map((position) => {
-      const children = input.nodes[position];
-      return step === "L" ? children[0] : children[1];
-    });
-    steps++;
-    stepIndex = (stepIndex + 1) % input.path.length;
+  while (true) {
+    if (pathers.every((pos) => isPositionGoal(pos.position))) {
+      return stepCount;
+    }
+
+    if (
+      pathers.every((pather) => pather.recurringGoalEncounters !== undefined)
+    ) {
+      return calculateFirstCommonEncounterAmongPathers(
+        pathers.map((p) => Array.from(p.recurringGoalEncounters!.values())),
+      );
+    }
+
+    const step = input.path[stepPathIndex];
+    stepCount++;
+    stepPathIndex = (stepPathIndex + 1) % input.path.length;
+    for (const pather of pathers) {
+      const children = input.nodes[pather.position];
+      pather.position = step === "L" ? children[0] : children[1];
+
+      if (!pather.recurringGoalEncounters && isPositionGoal(pather.position)) {
+        const firstEncounterStep = pather.firstGoalEncounters.get(
+          pather.position,
+        );
+        if (firstEncounterStep === undefined) {
+          pather.firstGoalEncounters.set(pather.position, stepCount);
+        } else {
+          pather.recurringGoalEncounters = new Map(
+            Array.from(pather.firstGoalEncounters)
+              .map(([otherPosition, otherFirstEncounterStep]) => [
+                otherPosition,
+                {
+                  firstEncounterStep: otherFirstEncounterStep,
+                  stepsUntilRevisit: stepCount - firstEncounterStep,
+                },
+              ]),
+          );
+        }
+      }
+    }
   }
-
-  return steps;
 }
 
 if (import.meta.main) {
   runPart(2023, 8, 1, part1);
-  // runPart(2023, 8, 2, part2);
+  runPart(2023, 8, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -98,6 +220,21 @@ LR
 XXX = (XXX, XXX)
 `;
 
+const TEST_INPUT4 = `\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22M, 22M)
+22M = (22N, 22N)
+22N = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+`;
+
 Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 2);
   assertEquals(part1(TEST_INPUT2), 6);
@@ -105,4 +242,5 @@ Deno.test("part1", () => {
 
 Deno.test("part2", () => {
   assertEquals(part2(TEST_INPUT3), 6);
+  assertEquals(part2(TEST_INPUT4), 8);
 });
