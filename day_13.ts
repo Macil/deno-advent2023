06313c97431ba4552704a0cd7415f7430e34264c
Chris Cowan <agentme49@gmail.com>
2024-10-15 02:50:22 -0700

    day 13 part 2

--- day_13.ts
+++ day_13.ts
@@ -69,14 +69,80 @@ function part1(input: string): number {
   return patterns.map(summarizePattern).reduce((a, b) => a + b, 0);
 }
 
-// function part2(input: string): number {
-//   const patterns = parse(input);
-//   throw new Error("TODO");
-// }
+function checkVerticalReflectionPart2(
+  pattern: Pattern,
+  index: number,
+): boolean {
+  const comparisonLength = Math.min(index, pattern.lines[0].length - index);
+  let incorrectTiles = 0;
+  for (const line of pattern.lines) {
+    for (let x = index - comparisonLength; x < index; x++) {
+      if (line[x] !== line[index + index - x - 1]) {
+        if (++incorrectTiles > 1) {
+          return false;
+        }
+      }
+    }
+  }
+  return incorrectTiles === 1;
+}
+
+function findVerticalReflectionPart2(pattern: Pattern): number | undefined {
+  for (let i = 1; i < pattern.lines[0].length; i++) {
+    if (checkVerticalReflectionPart2(pattern, i)) {
+      return i;
+    }
+  }
+  return undefined;
+}
+
+function checkHorizontalReflectionPart2(
+  pattern: Pattern,
+  index: number,
+): boolean {
+  const comparisonLength = Math.min(index, pattern.lines.length - index);
+  let incorrectTiles = 0;
+  for (let x = 0; x < pattern.lines[0].length; x++) {
+    for (let y = index - comparisonLength; y < index; y++) {
+      if (pattern.lines[y][x] !== pattern.lines[index + index - y - 1][x]) {
+        if (++incorrectTiles > 1) {
+          return false;
+        }
+      }
+    }
+  }
+  return incorrectTiles === 1;
+}
+
+function findHorizontalReflectionPart2(pattern: Pattern): number | undefined {
+  for (let i = 1; i < pattern.lines.length; i++) {
+    if (checkHorizontalReflectionPart2(pattern, i)) {
+      return i;
+    }
+  }
+  return undefined;
+}
+
+function summarizePatternPart2(pattern: Pattern): number {
+  const verticalReflectionIndex = findVerticalReflectionPart2(pattern);
+  if (verticalReflectionIndex !== undefined) {
+    return verticalReflectionIndex;
+  }
+  const horizontalReflectionIndex = findHorizontalReflectionPart2(pattern);
+  if (horizontalReflectionIndex !== undefined) {
+    return horizontalReflectionIndex * 100;
+  }
+  throw new Error("No reflection found");
+}
+
+function part2(input: string): number {
+  const patterns = parse(input);
+  return patterns.map(summarizePatternPart2).reduce((a, b) => a + b, 0);
+}
 
 if (import.meta.main) {
   runPart(2023, 13, 1, part1);
-  // runPart(2023, 13, 2, part2);
+  runPart(2023, 13, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -101,6 +167,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 405);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 400);
+});
