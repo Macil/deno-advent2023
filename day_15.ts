035e44bce6f5e267c9d1fab23d89a182eed42aff
Chris Cowan <agentme49@gmail.com>
2024-10-17 23:47:08 -0700

    day 15 part 2

--- day_15.ts
+++ day_15.ts
@@ -14,14 +14,66 @@ function part1(input: string): number {
   return input.trimEnd().split(",").map(hash).reduce((a, b) => a + b, 0);
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+interface Lens {
+  label: string;
+  focalLength: number;
+}
+
+interface Step {
+  lens: Lens;
+  operation: "=" | "-";
+}
+
+function parseSteps(input: string): Step[] {
+  return input.trimEnd().split(",").map((step): Step => {
+    const [label, operation, focalLength] = step.split(/([-=])/);
+    return {
+      lens: {
+        label,
+        focalLength: parseInt(focalLength),
+      },
+      operation: operation as "=" | "-",
+    };
+  });
+}
+
+function scoreBoxes(boxes: Map<number, Lens[]>): number {
+  return boxes.entries()
+    .filter(([_, lenses]) => lenses.length > 0)
+    .map(([boxNumber, lenses]) =>
+      lenses
+        .map((lens, i) => (1 + boxNumber) * (1 + i) * lens.focalLength)
+        .reduce((a, b) => a + b, 0)
+    )
+    .reduce((a, b) => a + b, 0);
+}
+
+function part2(input: string): number {
+  const steps = parseSteps(input);
+  const boxes = new Map<number, Lens[]>();
+  for (const step of steps) {
+    const labelHash = hash(step.lens.label);
+    let itemsInBox = boxes.get(labelHash) ?? [];
+    if (step.operation === "=") {
+      const existingLensIndexWithSameLabel = itemsInBox.findIndex(
+        (item) => item.label === step.lens.label,
+      );
+      if (existingLensIndexWithSameLabel !== -1) {
+        itemsInBox[existingLensIndexWithSameLabel] = step.lens;
+      } else {
+        itemsInBox.push(step.lens);
+      }
+    } else {
+      itemsInBox = itemsInBox.filter((item) => item.label !== step.lens.label);
+    }
+    boxes.set(labelHash, itemsInBox);
+  }
+  return scoreBoxes(boxes);
+}
 
 if (import.meta.main) {
   runPart(2023, 15, 1, part1);
-  // runPart(2023, 15, 2, part2);
+  runPart(2023, 15, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -32,6 +84,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 1320);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 145);
+});
